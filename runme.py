c48009ccaa6c0f088f22b1daac9edeb860dfab3d
Glitch752 <xxGlitch752xx@gmail.com>
2022-09-06 20:18:31 -0500

    Added intro animation

--- runme.py
+++ runme.py
@@ -5,6 +5,8 @@ from pyglet.window import key
 
 from pyglet import shapes
 
+import random
+
 window = pyglet.window.Window(width = 1520, height = 680, resizable = True, caption="Stranded")
 
 x = 0
@@ -17,6 +19,9 @@ oxygen = 1
 
 oxygenMinutes = 20
 oxygenDepletePerSecond = 1 / (oxygenMinutes * 60)
+
+gameState = "intro"
+introElapsed = 0
 
 pyglet.font.add_file("assets/fonts/Poppins-Regular.ttf")
 pyglet.font.load("Poppins")
@@ -69,8 +74,195 @@ for staticAsset in staticAssets:
 
 noise = PerlinNoise(octaves = 1, seed = 1000)
 
+starCount = 80
+
+stars = []
+
+shipPath = [
+    { "x": 0.74, "y": 0.820 },
+    { "x": 0.72, "y": 0.870 },
+    { "x": 0.70, "y": 0.900 },
+    { "x": 0.68, "y": 0.930 },
+    { "x": 0.66, "y": 0.950 },
+    { "x": 0.64, "y": 0.970 },
+    { "x": 0.62, "y": 0.980 },
+    { "x": 0.60, "y": 0.988 },
+    { "x": 0.58, "y": 0.990 },
+    { "x": 0.56, "y": 0.995 },
+    { "x": 0.54, "y": 0.993 },
+    { "x": 0.52, "y": 0.991 },
+    { "x": 0.50, "y": 0.987 },
+    { "x": 0.48, "y": 0.982 },
+    { "x": 0.46, "y": 0.978 },
+    { "x": 0.44, "y": 0.968 },
+    { "x": 0.42, "y": 0.958 },
+    { "x": 0.40, "y": 0.942 },
+    { "x": 0.38, "y": 0.920 },
+    { "x": 0.36, "y": 0.885 },
+    { "x": 0.34, "y": 0.845 },
+    { "x": 0.32, "y": 0.780 },
+    { "x": 0.31, "y": 0.700 },
+    { "x": 0.31, "y": 0.650 },
+    { "x": 0.32, "y": 0.580 },
+    { "x": 0.34, "y": 0.525 },
+    { "x": 0.37, "y": 0.500 },
+]
+
+soundNames = ["alarm", "blip", "step"]
+
+sounds = {}
+
+for soundName in soundNames:
+    sounds[soundName] = pyglet.media.load("assets/audio/" + soundName + ".wav", streaming=False)
+
+for i in range(starCount):
+    stars.append({
+        "x": random.random(),
+        "y": random.random()
+    })
+
 @window.event
 def on_draw():
+    global gameState
+    if gameState == "running":
+        drawGame()
+    elif gameState == "intro":
+        drawIntro()
+
+timeSinceAudioPlayed = 0
+dotsShown = 0
+
+def drawIntro():
+    global gameState, introElapsed, timeSinceAudioPlayed, dotsShown
+    
+    window.clear()
+
+    if introElapsed < 10:
+        print("TODO: Remove this after testing")
+        introElapsed = 10
+
+        label = pyglet.text.Label(str(math.ceil(5 - introElapsed / 2)),
+            font_name = "Poppins",
+            font_size = 200,
+            x = window.width / 2,
+            y = window.height / 2,
+            color = (255, 255, 255, round((((2 - introElapsed) / 2) % 2) * 255)),
+            anchor_x = "center", anchor_y = "center")
+            
+        label.draw()
+    elif introElapsed <= 10.5:
+        pass
+    elif introElapsed > 10.5 and introElapsed < 12.5:
+        drawConsole()
+
+        cover = shapes.Rectangle(
+            x = 0,
+            y = 0,
+            width = window.width,
+            height = window.height,
+            color = ( 0, 0, 0 )
+        )
+        cover.opacity = (2 - (introElapsed - 10.5)) * (255 / 2)
+        cover.draw()
+    elif introElapsed < 20.5:
+        drawConsole()
+        if dotsShown < len(shipPath):
+            if timeSinceAudioPlayed > 1 - ((introElapsed - 12.5) / 5):
+                sounds["blip"].play()
+                timeSinceAudioPlayed = 0
+                dotsShown += 1
+        else:
+            if timeSinceAudioPlayed > 0.5:
+                sounds["alarm"].play()
+                timeSinceAudioPlayed = 0
+        if introElapsed >= 18.5:
+            cover = shapes.Rectangle(
+                x = 0,
+                y = 0,
+                width = window.width,
+                height = window.height,
+                color = ( 0, 0, 0 )
+            )
+            cover.opacity = (introElapsed - 16.5) * (255 / 2)
+            cover.draw()
+    else:
+        gameState = "running"
+
+        
+    label = pyglet.text.Label("Space to skip",
+        font_name = "Poppins",
+        font_size = 20,
+        x = window.width / 2,
+        y = 0,
+        color = (255, 255, 255, 100),
+        anchor_x = "center", anchor_y = "bottom")
+        
+    label.draw()
+
+def drawConsole():
+    global dotsShown
+
+    consoleback = shapes.Rectangle(
+        x = 15,
+        y = 15,
+        width = window.width - 30,
+        height = window.height - 30,
+        color = ( 20, 20, 40 )
+    )
+    consoleback.draw()
+
+    consolescreen = shapes.Rectangle(
+        x = 30,
+        y = 30,
+        width = window.width - 60,
+        height = window.height - 60,
+        color = ( 0, 0, 0 )
+    )
+    consolescreen.draw()
+
+    for star in stars:
+        planet = shapes.Circle(
+            x = star["x"] * (window.width - 100) + 50,
+            y = star["y"] * (window.height - 100) + 50,
+            radius = 5,
+            color = ( 200, 200, 200 )
+        )
+        planet.draw()
+    
+    ship = shapes.Circle(
+        x = window.width / 4 * 3,
+        y = window.height / 4 * 3,
+        radius = 20,
+        color = ( 40, 40, 100 )
+    )
+    ship.draw()
+
+    index = 0
+    for point in shipPath:
+        index += 1
+        if(index > dotsShown):
+            continue
+        
+        pathPointColor = ( 150, 255, 150 )
+        if dotsShown == len(shipPath):
+            pathPointColor = ( 255, 150, 150 )
+        shipPathPoint = shapes.Circle(
+            x = point["x"] * (window.width - 100) + 50,
+            y = point["y"] * (window.height - 100) + 50,
+            radius = 5,
+            color = pathPointColor
+        )
+        shipPathPoint.draw()
+        
+    planet = shapes.Circle(
+        x = window.width / 2,
+        y = window.height / 2,
+        radius = 200,
+        color = ( 100, 40, 40 )
+    )
+    planet.draw()
+
+def drawGame():
     global walkingFrame, oxygen
 
     window.clear()
@@ -198,33 +390,40 @@ keysPressed = {
 
 @window.event
 def on_key_press(symbol, modifiers):
-    if symbol == key.LEFT:
-        print("The left arrow key was pressed.")
-        keysPressed["left"] = True
-    elif symbol == key.RIGHT:
-        print("The right arrow key was pressed.")
-        keysPressed["right"] = True
-    elif symbol == key.UP:
-        print("The up arrow key was pressed.")
-        keysPressed["up"] = True
-    elif symbol == key.DOWN:
-        print("The down arrow key was pressed.")
-        keysPressed["down"] = True
+    global gameState
+
+    if gameState == "running":
+        if symbol == key.LEFT:
+            print("The left arrow key was pressed.")
+            keysPressed["left"] = True
+        elif symbol == key.RIGHT:
+            print("The right arrow key was pressed.")
+            keysPressed["right"] = True
+        elif symbol == key.UP:
+            print("The up arrow key was pressed.")
+            keysPressed["up"] = True
+        elif symbol == key.DOWN:
+            print("The down arrow key was pressed.")
+            keysPressed["down"] = True
+    elif gameState == "intro":
+        if symbol == key.SPACE:
+            gameState = "running"
 
 @window.event
 def on_key_release(symbol, modifiers):
-    if symbol == key.LEFT:
-        print("The left arrow key was released.")
-        keysPressed["left"] = False
-    elif symbol == key.RIGHT:
-        print("The right arrow key was released.")
-        keysPressed["right"] = False
-    elif symbol == key.UP:
-        print("The up arrow key was released.")
-        keysPressed["up"] = False
-    elif symbol == key.DOWN:
-        print("The down arrow key was released.")
-        keysPressed["down"] = False
+    if gameState == "running":
+        if symbol == key.LEFT:
+            print("The left arrow key was released.")
+            keysPressed["left"] = False
+        elif symbol == key.RIGHT:
+            print("The right arrow key was released.")
+            keysPressed["right"] = False
+        elif symbol == key.UP:
+            print("The up arrow key was released.")
+            keysPressed["up"] = False
+        elif symbol == key.DOWN:
+            print("The down arrow key was released.")
+            keysPressed["down"] = False
 
 from pyglet.window import mouse
 
@@ -241,6 +440,15 @@ def on_mouse_press(x, y, button, modifiers):
 framerate = 144 # Frames per second
 
 def update(dt):
+    global introElapsed, timeSinceAudioPlayed
+    
+    if gameState == "running":
+        updateGame(dt)
+    elif gameState == "intro":
+        introElapsed += 1 / framerate
+        timeSinceAudioPlayed += 1 / framerate
+
+def updateGame(dt):
     global x, y, xMomentum, yMomentum, oxygen
 
     movementSpeed = 60 * dt
